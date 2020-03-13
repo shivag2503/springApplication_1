3b3673f96fc9f9aa6b89db3f61b285ce1815a383
Shivam Agrawal <shagra26@in.ibm.com>
2020-03-13 17:00:37 +0530

    files added

---
+++ src/main/java/com/shivam/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shivam/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
